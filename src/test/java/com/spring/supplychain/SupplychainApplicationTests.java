cb9c6df661e45ad52b9dd9404c1b5c6812718a46
ridasersif <ridasersif1@gmail.com>
2025-11-06 10:36:40 +0100

    Initialize supply chain project with Maven setup and basic application structure

---
+++ src/test/java/com/spring/supplychain/SupplychainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.supplychain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplychainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
