75800a74062866f6c363c51bd6de047c8a0bf87a
ridasersif <ridasersif1@gmail.com>
2025-11-07 08:46:35 +0100

    Add JPA auditing configuration and necessary dependencies in pom.xml

---
+++ src/main/java/com/spring/supplychain/config/JpaAuditingConfig.java
@@ -0,0 +1,17 @@
+package com.spring.supplychain.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> myauditorProvider() {
+        return () -> Optional.of("system");
+    }
+}
